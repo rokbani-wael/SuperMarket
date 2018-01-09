d151701677d1eff394d3e9d405b97f7f24777442
rokbani-wael <rokbani.wael@gmail.com>
2018-01-09 12:03:22 +0100

    Create TODO.cs

---
+++ TODO.cs
@@ -0,0 +1,15 @@
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scannez les produits >");
+            //AABBCC...
+            var scannedProducts = Console.ReadLine();
+            
+            //TODO
+
+            var total = 0;
+            Console.WriteLine(total);
+            Console.ReadLine();
+        }
+    }
